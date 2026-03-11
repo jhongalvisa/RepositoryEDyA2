b46e0f4e3fb6a3c2fcda89231a1a282b6548d7ba
jhongalvisa <jhon.galvis@uao.edu.co>
2026-03-11 16:58:05 -0500

    agregacionchallenge04

---
+++ Challenge 04/src/App.tsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import LibrosUI from './LibrosUI'
+
+export default function App() {
+  return (
+    <>
+      <div>
+        <LibrosUI/>
+
+        <hr/>
+      
+      </div>
+      
+    </>
+  )
+}
+
+
---
+++ Challenge 04/src/StackLibros.tsx
@@ -0,0 +1,34 @@
+import type { Libro } from "./LibrosType";
+
+export class StackLibros {
+    items: Libro[];
+    constructor() {
+        this.items = [];
+    }
+
+
+    push(libro: Libro) {
+        this.items.push(libro);
+    }
+
+    pop() {
+        return this.items.length > 0 ? this.items.pop() : null;
+    }
+
+    peek() {
+        return this.items.length > 0 ? this.items[this.items.length -1 ] : null;
+    }
+
+    isEmpty() {
+        return this.items.length === 0;
+    }
+
+    size() {
+        return this.items.length;
+    }
+
+    print(): Libro[] {
+        const arr = this.items.slice().reverse();
+        return arr;
+}
+}
